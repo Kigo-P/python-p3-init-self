96ccfacb7c8ca31702d9be55261c59d55572578c
Kigo-P <peterkigo33@gmail.com>
2024-08-30 21:28:37 +0300

    uses object initialization

--- lib/dog.py
+++ lib/dog.py
@@ -1,4 +1,13 @@
 #!/usr/bin/env python3
 
 class Dog:
+    
+    def __init__(self, name, breed = "Mutt"):
+        self.name = name
+        self.breed = breed
     pass
+
+
+hulk = Dog("hitler")
+print(hulk.name)
+print(hulk.breed)
--- lib/person.py
+++ lib/person.py
@@ -1,4 +1,10 @@
 #!/usr/bin/env python3
 
 class Person:
+
+    def __init__(self, name):
+        self.name = name
     pass
+
+person1 = Person("Peter")
+print(person1.name)
